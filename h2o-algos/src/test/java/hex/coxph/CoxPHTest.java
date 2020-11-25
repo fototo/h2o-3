87a5bb850f0f5bbc25af0689989239fa47eea971
Ondra Nekola <ondrej.nekola@h2o.ai>
2020-11-25 15:22:27 +0100

    CoxPH test using H2ORunner and Scope

--- h2o-algos/src/test/java/hex/coxph/CoxPHTest.java
+++ h2o-algos/src/test/java/hex/coxph/CoxPHTest.java
@@ -1,29 +1,30 @@
 package hex.coxph;
 
 import hex.StringPair;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import water.*;
 import water.fvec.*;
+import water.runner.CloudSize;
+import water.runner.H2ORunner;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.Assert.*;
+import static water.TestUtil.parse_and_track_test_file;
 
-public class CoxPHTest extends TestUtil {
-
-  @BeforeClass
-  public static void setup() { stall_till_cloudsize(1); }
+@RunWith(H2ORunner.class)
+@CloudSize(1)
+public class CoxPHTest extends Iced<CoxPHTest> {
 
   @Test
   public void testCoxPHEfron1Var() {
-    CoxPHModel model = null;
-    Frame fr = null;
     try {
-      fr = parse_test_file("smalldata/coxph_test/heart.csv");
+      Scope.enter();
+      final Frame fr = parse_and_track_test_file("smalldata/coxph_test/heart.csv");
 
-      CoxPHModel.CoxPHParameters parms = new CoxPHModel.CoxPHParameters();
+      final CoxPHModel.CoxPHParameters parms = new CoxPHModel.CoxPHParameters();
       parms._calc_cumhaz = true;
       parms._train           = fr._key;
       parms._start_column    = "start";
@@ -33,8 +34,9 @@ public class CoxPHTest extends TestUtil {
       parms._ties = CoxPHModel.CoxPHParameters.CoxPHTies.efron;
       assertEquals("Surv(start, stop, event) ~ age", parms.toFormula(fr));
 
-      CoxPH builder = new CoxPH(parms);
-      model = builder.trainModel().get();
+      final CoxPH builder = new CoxPH(parms);
+      final CoxPHModel model = builder.trainModel().get();
+      Scope.track_generic(model);
 
       assertEquals(model._output._coef[0],        0.0307077486571334,   1e-8);
       assertEquals(model._output._var_coef[0][0], 0.000203471477951459, 1e-8);
@@ -48,10 +50,7 @@ public class CoxPHTest extends TestUtil {
       assertEquals(model._output._wald_test,      4.6343882547245,      1e-8);
       assertEquals(model._output._var_cumhaz_2_matrix.rows(), 110);
     } finally {
-      if (fr != null)
-        fr.delete();
-      if (model != null)
-        model.delete();
+      Scope.exit();
     }
   }
 
@@ -59,7 +58,7 @@ public class CoxPHTest extends TestUtil {
   public void testCoxPHEfron1VarScoring() {
     try {
       Scope.enter();
-      Frame fr = Scope.track(parse_test_file("smalldata/coxph_test/heart.csv"));
+      final Frame fr = parse_and_track_test_file("smalldata/coxph_test/heart.csv");
 
       CoxPHModel.CoxPHParameters parms = new CoxPHModel.CoxPHParameters();
       parms._calc_cumhaz = true;
@@ -71,11 +70,11 @@ public class CoxPHTest extends TestUtil {
       parms._ties = CoxPHModel.CoxPHParameters.CoxPHTies.efron;
       assertEquals("Surv(start, stop, event) ~ age", parms.toFormula(fr));
 
-      CoxPH builder = new CoxPH(parms);
-      CoxPHModel model = (CoxPHModel) Scope.track_generic(builder.trainModel().get());
+      final CoxPH builder = new CoxPH(parms);
+      final CoxPHModel model = (CoxPHModel) Scope.track_generic(builder.trainModel().get());
 
       assertNotNull(model);
-      Frame linearPredictors = Scope.track(model.score(fr));
+      final Frame linearPredictors = Scope.track(model.score(fr));
       assertEquals(fr.numRows(), linearPredictors.numRows());
     } finally {
       Scope.exit();
@@ -84,12 +83,11 @@ public class CoxPHTest extends TestUtil {
 
   @Test
   public void testCoxPHBreslow1Var()  {
-    CoxPHModel model = null;
-    Frame fr = null;
     try {
-      fr = parse_test_file("smalldata/coxph_test/heart.csv");
+      Scope.enter();
+      final Frame fr = parse_and_track_test_file("smalldata/coxph_test/heart.csv");
 
-      CoxPHModel.CoxPHParameters parms = new CoxPHModel.CoxPHParameters();
+      final CoxPHModel.CoxPHParameters parms = new CoxPHModel.CoxPHParameters();
       parms._calc_cumhaz = true;
       parms._train           = fr._key;
       parms._start_column    = "start";
@@ -99,8 +97,9 @@ public class CoxPHTest extends TestUtil {
       parms._ties = CoxPHModel.CoxPHParameters.CoxPHTies.breslow;
       assertEquals("Surv(start, stop, event) ~ age", parms.toFormula(fr));
 
-      CoxPH builder = new CoxPH(parms);
-      model = builder.trainModel().get();
+      final CoxPH builder = new CoxPH(parms);
+      final CoxPHModel model = builder.trainModel().get();
+      Scope.track_generic(model);
 
       assertEquals(model._output._coef[0],        0.0306910411003801,   1e-8);
       assertEquals(model._output._var_coef[0][0], 0.000203592486905101, 1e-8);
@@ -114,21 +113,17 @@ public class CoxPHTest extends TestUtil {
       assertEquals(model._output._wald_test,      4.62659510743282,     1e-8);
       assertEquals(model._output._var_cumhaz_2_matrix.rows(), 110);
     } finally {
-      if (fr != null)
-        fr.delete();
-      if (model != null)
-        model.delete();
+      Scope.exit();
     }
   }
 
   @Test
   public void testCoxPHEfron1VarNoStart() {
-    CoxPHModel model = null;
-    Frame fr = null;
     try {
-      fr = parse_test_file("smalldata/coxph_test/heart.csv");
+      Scope.enter();
+      final Frame fr = parse_and_track_test_file("smalldata/coxph_test/heart.csv");
 
-      CoxPHModel.CoxPHParameters parms = new CoxPHModel.CoxPHParameters();
+      final CoxPHModel.CoxPHParameters parms = new CoxPHModel.CoxPHParameters();
       parms._calc_cumhaz = true;
       parms._train           = fr._key;
       parms._start_column    = null;
@@ -139,7 +134,8 @@ public class CoxPHTest extends TestUtil {
       assertEquals("Surv(stop, event) ~ age", parms.toFormula(fr));
 
       CoxPH builder = new CoxPH(parms);
-      model = builder.trainModel().get();
+      final CoxPHModel model = builder.trainModel().get();
+      Scope.track_generic(model);
 
       assertEquals(model._output._coef[0],        0.0289468187293998,   1e-8);
       assertEquals(model._output._var_coef[0][0], 0.000210975113029285, 1e-8);
@@ -153,21 +149,17 @@ public class CoxPHTest extends TestUtil {
       assertEquals(model._output._wald_test,      3.97164529276219,     1e-8);
       assertEquals(model._output._var_cumhaz_2_matrix.rows(), 110);
     } finally {
-      if (fr != null)
-        fr.delete();
-      if (model != null)
-        model.delete();
+      Scope.exit();
     }
   }
 
   @Test
   public void testCoxPHBreslow1VarNoStart() {
-    CoxPHModel model = null;
-    Frame fr = null;
     try {
-      fr = parse_test_file("smalldata/coxph_test/heart.csv");
+      Scope.enter();
+      final Frame fr = parse_and_track_test_file("smalldata/coxph_test/heart.csv");
 
-      CoxPHModel.CoxPHParameters parms = new CoxPHModel.CoxPHParameters();
+      final CoxPHModel.CoxPHParameters parms = new CoxPHModel.CoxPHParameters();
       parms._calc_cumhaz = true;
       parms._train           = fr._key;
       parms._start_column    = null;
@@ -177,8 +169,9 @@ public class CoxPHTest extends TestUtil {
       parms._ties = CoxPHModel.CoxPHParameters.CoxPHTies.breslow;
       assertEquals("Surv(stop, event) ~ age", parms.toFormula(fr));
 
-      CoxPH builder = new CoxPH(parms);
-      model = builder.trainModel().get();
+      final CoxPH builder = new CoxPH(parms);
+      final CoxPHModel model = builder.trainModel().get();
+      Scope.track_generic(model);
 
       assertEquals(model._output._coef[0],        0.0289484855901731,   1e-8);
       assertEquals(model._output._var_coef[0][0], 0.000211028794751156, 1e-8);
@@ -192,10 +185,7 @@ public class CoxPHTest extends TestUtil {
       assertEquals(model._output._wald_test,      3.97109228128153,     1e-8);
       assertEquals(model._output._var_cumhaz_2_matrix.rows(), 110);
     } finally {
-      if (fr != null)
-        fr.delete();
-      if (model != null)
-        model.delete();
+      Scope.exit();
     }
   }
 
@@ -203,10 +193,10 @@ public class CoxPHTest extends TestUtil {
   public void testCoxPHEfron1Interaction() {
     try {
       Scope.enter();
-      Frame fr = Scope.track(parse_test_file("smalldata/coxph_test/heart.csv"));
+      final Frame fr = parse_and_track_test_file("smalldata/coxph_test/heart.csv");
 
       // Decompose a "age" column into two components: "age1" and "age2"
-      Frame ext = new MRTask() {
+      final Frame ext = new MRTask() {
         @Override
         public void map(Chunk c, NewChunk nc0, NewChunk nc1) {
           for (int i = 0; i < c._len; i++) {
@@ -258,12 +248,11 @@ public class CoxPHTest extends TestUtil {
 
   @Test
   public void testCoxPHSingleNodeMode() {
-    CoxPHModel model = null;
-    Frame fr = null;
     Key<Frame> rebalancedKey = Key.make();
     try {
-      fr = parse_test_file("smalldata/coxph_test/heart.csv");
-      fr = rebalanceToAllNodes(fr, rebalancedKey);
+      Scope.enter();
+      Frame fr = parse_and_track_test_file("smalldata/coxph_test/heart.csv");
+      fr = Scope.track(rebalanceToAllNodes(fr, rebalancedKey));
 
       CoxPHModel.CoxPHParameters parms = new CoxPHModel.CoxPHParameters();
       parms._auto_rebalance  = false; // make sure we keep the original frame layout
@@ -279,7 +268,8 @@ public class CoxPHTest extends TestUtil {
       System.setProperty("sys.ai.h2o.debug.checkRunLocal", Boolean.TRUE.toString());
       parms._single_node_mode = true;
       CoxPH builder = new CoxPH(parms);
-      model = builder.trainModel().get();
+      CoxPHModel model = builder.trainModel().get();
+      Scope.track_generic(model);
 
       assertEquals(model._output._coef[0],        0.0307077486571334,   1e-8);
       assertEquals(model._output._var_coef[0][0], 0.000203471477951459, 1e-8);
@@ -294,13 +284,7 @@ public class CoxPHTest extends TestUtil {
       assertEquals(model._output._var_cumhaz_2_matrix.rows(), 110);
     } finally {
       System.setProperty("sys.ai.h2o.debug.checkRunLocal", Boolean.FALSE.toString());
-      Frame rebalanced = rebalancedKey.get();
-      if (rebalanced != null)
-        rebalanced.delete();
-      if (fr != null)
-        fr.delete();
-      if (model != null)
-        model.delete();
+      Scope.exit();
     }
   }
 
@@ -337,14 +321,9 @@ public class CoxPHTest extends TestUtil {
 
   @Test
   public void testJavaScoring() {
-    Frame fr = null;
-    CoxPHModel dl = null;
-    CoxPHModel dl2 = null;
-
     try {
-      fr = parse_test_file("smalldata/coxph_test/heart.csv");
-
-
+      Scope.enter();
+      final Frame fr = parse_and_track_test_file("smalldata/coxph_test/heart.csv");
       CoxPHModel.CoxPHParameters parms = new CoxPHModel.CoxPHParameters();
       parms._calc_cumhaz = true;
       parms._train           = fr._key;
@@ -354,14 +333,13 @@ public class CoxPHTest extends TestUtil {
       parms._ignored_columns = new String[]{"id", "year", "surgery", "transplant"};
       parms._ties = CoxPHModel.CoxPHParameters.CoxPHTies.efron;
       
-      dl = new CoxPH(parms).trainModel().get();
-      Frame res = dl.score(fr);
+      final CoxPHModel dl = new CoxPH(parms).trainModel().get();
+      Scope.track_generic(dl);
+      Frame res = Scope.track(dl.score(fr));
       assertTrue(dl.testJavaScoring(fr, res, 1e-5));
       res.remove();
     } finally {
-      if (fr != null) fr.delete();
-      if (dl != null) dl.delete();
-      if (dl2 != null) dl2.delete();
+      Scope.exit();
     }
   }
 
